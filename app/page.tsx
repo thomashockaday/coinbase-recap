7f4ad7360cd9995bc298540f18bd9284be499540
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2023-10-02 12:00:30 +0100

    Add csv input form

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,39 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export default function Home() {
   return (
     <main className="p-24">
       <section className="py-12 flex flex-col items-center gap-8">
         <h1 className="text-4xl font-bold">Coinbase Recap</h1>
-        <p className="text-2xl text-muted-foreground">
-          View a breakdown of your Coinbase transaction history by uploading a
-          CSV
+
+        <p className="text-2xl text-muted-foreground text-center">
+          View a breakdown of your Coinbase transaction history
         </p>
+
+        <p className="max-w-2xl text-center">
+          To get started you will need to export your full Coinbase transaction
+          history as a CSV file. You can find instructions on how to do this on
+          the{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://help.coinbase.com/en/contact-us/support-faq/coinbase/how-to-view-your-balance-and-transactions"
+            className="text-primary underline hover:no-underline"
+          >
+            official Coinbase support site
+          </a>
+          .
+        </p>
+
+        <div className="w-full max-w-sm">
+          <Label htmlFor="csv">Upload your transaction history</Label>
+          <Input id="csv" type="file" />
+        </div>
+
+        <Button>View my recap</Button>
       </section>
-      <div className="flex gap-6 items-center justify-center">
-        <Button>Lets go</Button>
-      </div>
     </main>
   );
 }
