33d27104fb65eb002a3521c92f071ca241dad019
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2026-02-08 16:30:49 +0000

    Add disclaimer clarifying no data is stored

--- components/template/footer.tsx
+++ components/template/footer.tsx
@@ -4,5 +4,9 @@ export const Footer = () => (
       The information on this page is for general information only and is not
       affiliated with Coinbase in any way.
     </p>
+
+    <p className="text-muted-foreground text-xs text-center">
+      <strong>This site does not store any of your data.</strong>
+    </p>
   </footer>
 );
